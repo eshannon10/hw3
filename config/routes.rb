3ea5f678b9d6c7da77155457cbee1cbed52d2d3d
Ellie Shannon <ellie.shannon@kellogg.northwestern.edu>
2025-02-22 23:28:17 +0000

    created places index page and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  #shorthand for setting up index, show, new, and create
+  resources :places
+  resources :entries
+
+  #sets the homepage to the index action of the Places controller, aka the list of places (didn't specifically cover in class but good practice)
+  root "places#index"
+  
+
 end
